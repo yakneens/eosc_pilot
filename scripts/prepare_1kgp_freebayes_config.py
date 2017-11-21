28a5338cc5a8e8e36f3fe8b6b2e7ef0121632900
Sergei Iakhnin <llevar@gmail.com>
2017-11-21 14:57:31 +0100

    Script to generate analysis runs for 1kgp germline calling

--- scripts/prepare_1kgp_freebayes_config.py
+++ scripts/prepare_1kgp_freebayes_config.py
@@ -1,38 +1,60 @@
 import os.path
 import datetime
 import re
+import json
+import uuid
+import argparse
 
-samples_path = "/root/oneclient/EOSCpilot-PanCancer/data"
 release_1_date = "20120522"
 release_2_date = "20130415"
 filename_re = ".*\.mapped.*{}\.bam$"
 counter = 0
 
-def write_config_to_file(config, config_location):
+def parse_args():
+    my_parser = argparse.ArgumentParser()
     
-    run_uuid = str(uuid.uuid4())
+    sub_parsers = my_parser.add_subparsers()
     
-    my_file = open("{}/{}.json".format(config_location, run_uuid), "w")
-    json.dump(config, my_file)
-    my_file.close()
+    create_configs_parser = sub_parsers.add_parser("create-configs", conflict_handler='resolve')
+    create_configs_parser.add_argument("-c", "--config_location", help="Path to a directory where the generated config files will be stored.", dest="config_location", required=True)
+    create_configs_parser.add_argument("-s", "--samples_path", help="Path to a directory where the samples are.", dest="samples_path", required=True)
+    create_configs_parser.set_defaults(func=create_configs_command)
+    
+    my_args = my_parser.parse_args()
+    
+    return my_args
 
-def generate_config_objects(available_samples, num_runs, config_location):
-    for this_run in range(num_runs):
-    
-        
-        this_config_data = {"sample": {
-                                "donor_index": available_samples[this_run].index,
-                                "sample_id": available_samples[this_run].sample_id.split(",")[0],
-                                "sample_location": available_samples[this_run].sample_location
+
+def generate_config_objects(samples_path, config_location):
+    for root, dirs, files in os.walk(samples_path):
+        for my_file in files:
+            if re.match(filename_re.format(release_1_date), my_file) or re.match(filename_re.format(release_2_date), my_file):
+                this_config_data = {"sample": {
+                                    "sample_id": os.path.basename(os.path.dirname(root)),
+                                    "sample_location": os.path.join(root, my_file)
+                                    }
                                 }
-                            }
+    
+                run_uuid = str(uuid.uuid4())
+    
+                my_file = open("{}/{}.json".format(config_location, run_uuid), "w")
+                json.dump(this_config_data, my_file)
+                my_file.close()
+
+
+def create_configs_command(args):
+
+    config_location = args.config_location
+    samples_path = args.samples_path
+
+    if (not os.path.isdir(config_location)):
+        os.makedirs(config_location)
         
-        yield this_config_data
+    generate_config_objects(samples_path, config_location)
+    
+if __name__ == '__main__':
+    args = parse_args()
+    args.func(args)
 
-
-for root, dirs, files in os.walk(samples_path):
-    for my_file in files:
-        if re.match(filename_re.format(release_1_date), my_file):
-            print os.path.join(root, my_file)
 
 
